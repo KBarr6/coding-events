70763b34cee2541510a4c146c8235548e95881c4
KBarr <ksamuels6@yahoo.com>
2020-09-29 02:39:29 -0500

    chapter 12 starting exercise

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,11 +1,10 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,24 +19,25 @@ public class EventController {
 
     //private static List<String> events = new ArrayList<>();
 
-//    @GetMapping
-//    public String displayAllEvents(Model model) {
-//        model.addAttribute("events", events);
-//          return "events/index";
-//    }
-
-
-    private static HashMap<String, String> events = new HashMap<>();
-
     @GetMapping
-    public String displayAllEvents(Model model){
-        model.addAttribute("events", events);
-        return "events/index";
+    public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", EventData.getAll());
+          return "events/index";
     }
 
+
+   // private static HashMap<String, String> events = new HashMap<>();
+
+//    @GetMapping
+//    public String displayAllEvents(Model model){
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
+
     @PostMapping("create")
-    public String createAllEvents(@RequestParam String eventName, @RequestParam String eventDescription){
-        events.put(eventName, eventDescription);
+    public String processCreateEventsForm(@ModelAttribute Event newEvent){
+        EventData.add(newEvent);
            return "redirect:";
     }
 
@@ -53,6 +53,24 @@ public class EventController {
 //        return "redirect:"; //will redirect to /events
 //    }
 
+
+    @GetMapping("delete")
+        public String displayDeleteEventForm(Model model) {
+            model.addAttribute("title", "Delete Events");
+            model.addAttribute("events", EventData.getAll());
+            return "events/delete";
+            }// lives at /events/delete
+
+    @PostMapping("delete")
+        public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds){
+        if(eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+            return "redirect:";
+
+    }
     }
 
 
